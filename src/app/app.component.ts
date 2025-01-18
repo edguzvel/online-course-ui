50a93814f8ff045f7296f63b5fde1303c0468e00
Eduardo Guzman <edguzmanvel@gmail.com>
2025-01-18 07:21:01 -0600

    Navbar and carousel images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { NavBarComponent } from "./components/core/nav-bar/nav-bar.component";
 
+//A class decorated with 'compenent' then it becomes a component for angular
+// it has these
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', //this name is used as a component selection
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, NavBarComponent],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
